a28ebeda13fc90a7c37aeccad0e466083cff0f22
Ankit Kandulna <144378696+ANKIT-KANDULNA@users.noreply.github.com>
2025-09-02 03:43:44 +0530

    Add files via upload

---
+++ basicSpamEmail.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import svm
+
+# Loading Dataset
+dataframe=pd.read_csv("spam_ham_dataset.csv")
+print(dataframe.head())
+print(dataframe.describe())
+
+# Split into Training and Testing Data
+# 80% as training and remaining as testing
+x=dataframe["text"]
+y=dataframe["label"]
+x_train, y_train=x[0:4110],y[0:4110]
+x_test, y_test=x[4110:],y[4110:]
+
+# extract features
+cv=CountVectorizer()
+features=cv.fit_transform(x_train)
+
+# build a model
+model=svm.SVC()
+model.fit(features,y_train)
+
+# test accuracy
+feature_test=cv.transform(x_test)
+print("Model score is: ",model.score(feature_test,y_test))
